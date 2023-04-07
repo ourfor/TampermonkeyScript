bf9db018313a636a27bb840a04d040a0055d9bc8
ourfor <ourfor@qq.com>
2023-04-07 22:47:55 +0800

    ✨ update nplayer

--- nPlayer.js
+++ nPlayer.js
@@ -16,32 +16,48 @@
     'use strict';
     // Your code here...
     GM_log("nPlayer plugin")
-    function getToken() {
-        const token =  localStorage.getItem("token");
+    function getToken(isH5) {
+        const size = localStorage.length;
+        let key = "";
+        for (let i = 0; i < size; i++) {
+            key = localStorage.key(i);
+            if (key.endsWith("token")) {
+                break;
+            }
+        }
+        const token = localStorage.getItem(key);
         return token.substring(1, token.length-1);
     }
 
+    function log(args) {
+        GM_log(args)
+        console.log(args)
+    }
 
     const id = GM_registerMenuCommand ("使用nPlayer播放", () => {
-        GM_log("open with nPlayer")
+        log("open with nPlayer")
         const url = new URL(document.baseURI);
-        const videoId = parseInt(url.pathname.substring(url.pathname.lastIndexOf("/")+1));
-        const token = getToken();
+        const isH5 = url.pathname.includes("h5.")
+        let pathname = url.pathname;
+        if (pathname.endsWith("/0")) pathname = pathname.substring(0, pathname.length-2)
+        const videoId = parseInt(pathname.substring(pathname.lastIndexOf("/")+1));
+        const token = getToken(isH5);
         const apiUrl = `${url.origin}/api/videos/getPreUrl`
         const req = {
             method: "POST",
             url: apiUrl,
             headers: {
+              "Content-Type": "application/json; charset=utf-8",
+              "content-type": "application/json; charset=utf-8",
               "auth": token,
-              "origin": url.origin,
-              "Content-Type": "application/json"
+              "origin": url.origin
             },
             data: JSON.stringify({
                 videoId: videoId
             }),
             onload: function(response) {
-                console.log("video real play url")
-                console.log(response.responseText);
+                log("video real play url")
+                log(response.responseText);
 
                 // nplayer://
                 const resp = JSON.parse(response.responseText)
@@ -49,12 +65,11 @@
                 url.searchParams.delete("start")
                 url.searchParams.delete("end");
                 const link = url.href;
-                console.log(link)
-                GM_log(link)
+                log(link)
                 GM_openInTab(`nplayer://${link}`)
             }
         };
-        GM_log(req)
+        log(req)
         GM_xmlhttpRequest(req)
      }, "h");
 })();

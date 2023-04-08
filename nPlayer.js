1b866b92a15c06cf82f4fe96c090cbe2a34d2f08
ourfor <ourfor@qq.com>
2023-04-08 23:01:05 +0800

    ✨ update player

--- nPlayer.js
+++ nPlayer.js
@@ -11,11 +11,54 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_download
 // @grant        GM_openInTab
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 (function () {
     'use strict';
     // Your code here...
-    GM_log("nPlayer plugin")
+    function randomString(e) {    
+        e = e || 32;
+        const chs = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
+        const length = chs.length;
+        let n = "";
+        for (let i = 0; i < e; i++) n += chs.charAt(Math.floor(Math.random() * length));
+        return n
+    }
+    const helper = {
+        log: (...args) => {
+            GM_log(...args);
+            console.log(...args);
+        },
+        addMenu: (title, action, shotcut) => {
+            GM_registerMenuCommand(title, action, shotcut);
+        },
+        fetch: (...args) => {
+            GM_xmlhttpRequest(...args);
+        },
+        open: (...args) => {
+            GM_openInTab(...args);
+        },
+        read: (key, default_value) => {
+            return GM_getValue(key, default_value)
+        },
+        write: (key, value) => {
+            GM_setValue(key, value)
+        }
+    };
+
+    helper.log("nPlayer plugin")
+    function setToken(value) {
+        const size = localStorage.length;
+        let key = "";
+        for (let i = 0; i < size; i++) {
+            key = localStorage.key(i);
+            if (key.endsWith("token")) {
+                break;
+            }
+        }
+        localStorage.setItem(key, value);
+    }
     function getToken(isH5) {
         const size = localStorage.length;
         let key = "";
@@ -29,35 +72,63 @@
         return token.substring(1, token.length-1);
     }
 
-    function log(args) {
-        GM_log(args)
-        console.log(args)
-    }
+    helper.addMenu("刷新Token", () => {
+        const url = new URL(document.baseURI);
+        const apiUrl = `${url.origin}/api/login/userReg`
+        const mimeType = "application/json";
+        const username = `${randomString(10)}@mail.com`;
+        const password = "89LRBsPjz1IvE4w2eHIM+A=="
+        const req = {
+            method: "POST",
+            url: apiUrl,
+            headers: {
+              "Content-Type": mimeType,
+              "origin": url.origin
+            },
+            overrideMimeType: mimeType,
+            data: JSON.stringify({
+                "user_login": username,
+                "user_pass": password,
+                "source":"pc",
+                "channel":"P"
+            }),
+            onload: function(response) {
+                helper.log("refresh token")
+                helper.log(response.responseText);
+                const res = JSON.parse(response.responseText)
+                helper.write("token", res.data.token)
+                setToken(`"${res.data.token}"`)
+            }
+        };
+        helper.log(req)
+        helper.fetch(req);
+    }, "l")
 
-    const id = GM_registerMenuCommand ("使用nPlayer播放", () => {
-        log("open with nPlayer")
+    helper.addMenu("使用nPlayer播放", () => {
+        helper.log("open with nPlayer")
         const url = new URL(document.baseURI);
         const isH5 = url.pathname.includes("h5.")
         let pathname = url.pathname;
         if (pathname.endsWith("/0")) pathname = pathname.substring(0, pathname.length-2)
         const videoId = parseInt(pathname.substring(pathname.lastIndexOf("/")+1));
-        const token = getToken(isH5);
+        const token = helper.read("token");
         const apiUrl = `${url.origin}/api/videos/getPreUrl`
+        const mimeType = "application/json";
         const req = {
             method: "POST",
             url: apiUrl,
             headers: {
-              "Content-Type": "application/json; charset=utf-8",
-              "content-type": "application/json; charset=utf-8",
+              "Content-Type": mimeType,
               "auth": token,
               "origin": url.origin
             },
+            overrideMimeType: mimeType,
             data: JSON.stringify({
                 videoId: videoId
             }),
             onload: function(response) {
-                log("video real play url")
-                log(response.responseText);
+                helper.log("video real play url")
+                helper.log(response.responseText);
 
                 // nplayer://
                 const resp = JSON.parse(response.responseText)
@@ -65,12 +136,14 @@
                 url.searchParams.delete("start")
                 url.searchParams.delete("end");
                 const link = url.href;
-                log(link)
-                GM_openInTab(`nplayer://${link}`)
+                const nPlayerLink = `nplayer-${link}`;
+                helper.log(link)
+                helper.log(nPlayerLink);
+                helper.open(nPlayerLink)
             }
         };
-        log(req)
-        GM_xmlhttpRequest(req)
+        helper.log(req)
+        helper.fetch(req)
      }, "h");
 })();
 

98004eae87f7a06bbd54bb170c4c7c012af202f3
ourfor <ourfor@qq.com>
2023-04-10 20:45:36 +0800

    ✨ update player.js

--- nPlayer.js
+++ nPlayer.js
@@ -3,9 +3,13 @@
 // @namespace    https://stay.app/
 // @version      0.1
 // @icon         https://static.ourfor.top/app/nplayer/nplayer.png
-// @description  使用nPlayer播放
+// @description  使用外部播放器播放
 // @author       ourfor
 // @match        *://*/*
+// @require      https://cdn.plyr.io/3.7.8/plyr.js
+// @resource plyrStyle     https://cdn.plyr.io/3.7.8/plyr.css
+// @resource nPlayerIcon   https://static.ourfor.top/app/nplayer/nplayer.png
+// @resource plyrIcon      https://static.ourfor.top/app/plyr/plyr.png
 // @grant        GM_registerMenuCommand
 // @grant        GM_unregisterMenuCommand
 // @grant        GM_xmlhttpRequest
@@ -13,18 +17,14 @@
 // @grant        GM_openInTab
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_addStyle
+// @grant        GM_getResourceURL
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 // ==/UserScript==
 (function () {
     'use strict';
     // Your code here...
-    function randomString(e) {    
-        e = e || 32;
-        const chs = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
-        const length = chs.length;
-        let n = "";
-        for (let i = 0; i < e; i++) n += chs.charAt(Math.floor(Math.random() * length));
-        return n
-    }
     const helper = {
         log: (...args) => {
             GM_log(...args);
@@ -32,6 +32,20 @@
         },
         addMenu: (title, action, shotcut) => {
             GM_registerMenuCommand(title, action, shotcut);
+        },
+        addElement: (tag, option, parent = null) => {
+            if (parent) {
+                const element = document.createElement(tag, option)
+                parent.appendChild(element)
+                return element;
+            } else {
+                const element = document.createElement(tag, option)
+                document.body.appendChild(element)
+                return element;
+            }
+        },
+        addStyle: (css) => {
+            GM_addStyle(css)
         },
         fetch: (...args) => {
             GM_xmlhttpRequest(...args);
@@ -44,6 +58,15 @@
         },
         write: (key, value) => {
             GM_setValue(key, value)
+        },
+        randomString: (size = 32) => {
+            const chs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnoprstuvwxyz0123456789";
+            const length = chs.length;
+            let result = "";
+            for (let i = 0; i < size; i++) {
+                result += chs.charAt(Math.floor(Math.random() * length));
+            }
+            return result
         }
     };
 
@@ -59,6 +82,7 @@
         }
         localStorage.setItem(key, value);
     }
+
     function getToken(isH5) {
         const size = localStorage.length;
         let key = "";
@@ -69,30 +93,29 @@
             }
         }
         const token = localStorage.getItem(key);
-        return token.substring(1, token.length-1);
+        return token.substring(1, token.length - 1);
     }
-
-    helper.addMenu("刷新Token", () => {
+    function refreshToken() {
         const url = new URL(document.baseURI);
         const apiUrl = `${url.origin}/api/login/userReg`
         const mimeType = "application/json";
-        const username = `${randomString(10)}@mail.com`;
+        const username = `${helper.randomString(10)}@mail.com`;
         const password = "89LRBsPjz1IvE4w2eHIM+A=="
         const req = {
             method: "POST",
             url: apiUrl,
             headers: {
-              "Content-Type": mimeType,
-              "origin": url.origin
+                "Content-Type": mimeType,
+                "origin": url.origin
             },
             overrideMimeType: mimeType,
             data: JSON.stringify({
                 "user_login": username,
                 "user_pass": password,
-                "source":"pc",
-                "channel":"P"
+                "source": "pc",
+                "channel": "P"
             }),
-            onload: function(response) {
+            onload: function (response) {
                 helper.log("refresh token")
                 helper.log(response.responseText);
                 const res = JSON.parse(response.responseText)
@@ -102,15 +125,13 @@
         };
         helper.log(req)
         helper.fetch(req);
-    }, "l")
-
-    helper.addMenu("使用nPlayer播放", () => {
-        helper.log("open with nPlayer")
+    }
+    function realMediaSource(callback) {
         const url = new URL(document.baseURI);
         const isH5 = url.pathname.includes("h5.")
         let pathname = url.pathname;
-        if (pathname.endsWith("/0")) pathname = pathname.substring(0, pathname.length-2)
-        const videoId = parseInt(pathname.substring(pathname.lastIndexOf("/")+1));
+        if (pathname.endsWith("/0")) pathname = pathname.substring(0, pathname.length - 2)
+        const videoId = parseInt(pathname.substring(pathname.lastIndexOf("/") + 1));
         const token = helper.read("token");
         const apiUrl = `${url.origin}/api/videos/getPreUrl`
         const mimeType = "application/json";
@@ -118,32 +139,131 @@
             method: "POST",
             url: apiUrl,
             headers: {
-              "Content-Type": mimeType,
-              "auth": token,
-              "origin": url.origin
+                "Content-Type": mimeType,
+                "auth": token,
+                "origin": url.origin
             },
             overrideMimeType: mimeType,
             data: JSON.stringify({
                 videoId: videoId
             }),
-            onload: function(response) {
+            onload: function (response) {
                 helper.log("video real play url")
                 helper.log(response.responseText);
 
-                // nplayer://
                 const resp = JSON.parse(response.responseText)
                 const url = new URL(resp.data.url)
                 url.searchParams.delete("start")
                 url.searchParams.delete("end");
                 const link = url.href;
-                const nPlayerLink = `nplayer-${link}`;
-                helper.log(link)
-                helper.log(nPlayerLink);
-                helper.open(nPlayerLink)
+                callback(link)
             }
         };
         helper.log(req)
         helper.fetch(req)
-     }, "h");
+    }
+    function addPlayerContainer() {
+        if (document.plyrContainer) return;
+        const container = helper.addElement('div', {});
+        container.id = "plyr-video-container"
+        const video = helper.addElement('video', {}, container)
+        video.id = "plyr-video"
+        document.plyrContainer = container;
+    }
+    function initPlayer() {
+        if (window.plyrPlayer) return;
+        const data = {
+            type: "video",
+            captions: { active: true, update: true, language: "zh" },
+            sources: [
+              {
+                src: ""
+              }
+            ]
+          };
+          const player = new Plyr("#plyr-video", data);
+          window.plyrPlayer = player
+    }
+    function playWithPlyr() {
+        helper.log("play with plyr");
+        realMediaSource((link) => {
+            const player = window.plyrPlayer;
+            player.source = {
+                type: 'video',
+                sources: [
+                  { src: link },
+                ],
+            };
+            player.play();
+        })
+    }
+    helper.addMenu("刷新Token", refreshToken, "l")
+    function playWithNPlayer() {
+        helper.log("play with nPlayer")
+        realMediaSource((link) => {
+            const nPlayerLink = `nplayer-${link}`;
+            helper.log(link)
+            helper.log(nPlayerLink);
+            helper.open(nPlayerLink)
+        })
+    }
+    helper.addMenu("使用nPlayer播放", playWithNPlayer, "h");
+    const plyrStyle = helper.addElement('link', {type: "text/css"}, document.head);
+    plyrStyle.rel = "stylesheet";
+    plyrStyle.href = "https://cdn.plyr.io/3.7.8/plyr.css";
+    helper.addStyle(`
+     #player-menu {
+        display: flex;
+        position: fixed;
+        transform: translate(0, -50%);
+        min-height: 2em;
+        width: 2em;
+        background-color: rgba(241, 241, 241, 0.29);
+        top: 50%;
+        left: 0;
+        border-radius: 0.4rem;
+        flex-direction: column;
+        z-index: 9999;
+     }
+     #player-menu > img {
+        cursor: pointer;
+     }
+     #player-menu > img:first-child {
+        margin-top: 0.2em;
+     } 
+     #player-menu > img:last-child {
+        margin-top: 0.2em;
+     } 
+     #player-nPlayer, #player-plyr {
+        width: 100%;
+        margin-top: 0.1em;
+        margin-bottom: 0.1em;
+     }
+     #plyr-video-container {
+        position: fixed;
+        width: 90%;
+        min-height: 40%;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 9999;
+     } 
+    `)
+    const menuContainer = helper.addElement('div', {}, document.body)
+    menuContainer.id = "player-menu";
+    const nPlayerItem = helper.addElement('img', {}, menuContainer)
+    nPlayerItem.id = "player-nPlayer"
+    nPlayerItem.src = GM_getResourceURL("nPlayerIcon")
+    nPlayerItem.onclick = (e) => {
+        playWithNPlayer()
+    }
+    const plyrItem = helper.addElement('img', {}, menuContainer)
+    plyrItem.id = "player-plyr"
+    plyrItem.src = GM_getResourceURL("plyrIcon")
+    plyrItem.onclick = (e) => {
+        addPlayerContainer();
+        initPlayer();
+        playWithPlyr();
+    }
 })();
 

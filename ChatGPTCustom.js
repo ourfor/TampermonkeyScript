af48a59775a4ad17a55b1275caa1c98a2e10bf48
ourfor <ourfor@qq.com>
2023-04-23 19:45:17 +0800

    🐛 fix bug of surge match pattern

--- ChatGPTCustom.js
+++ ChatGPTCustom.js
@@ -6,10 +6,9 @@ if ($response.status == 200) {
         $done({ status: $request.status, headers: $response.headers, body: JSON.stringify(data) })
     } else if ($request.url.indexOf("chat.openai.com/backend-api/accounts/check") != -1) {
         const data = JSON.parse($response.body);
-        data.account_plan.was_paid_customer = true;
         data.account_plan.is_paid_subscription_active = true;
+        data.account_plan.was_paid_customer = true;
         $done({ status: $request.status, headers: $response.headers, body: JSON.stringify(data) })
-
     } else {
         $done({})
     }
@@ -17,4 +16,4 @@ if ($response.status == 200) {
     $done({})
 }
 
-// 💄 ChatGPT个性化 = type=http-response, pattern=^https:\/\/chat\.openai\.com\/api, script-path=https://raw.githubusercontent.com/ourfor/TampermonkeyScript/master/ChatGPTCustom.js, requires-body=true, timeout=10, enable=true
+// 💄 ChatGPT个性化 = type=http-response, pattern=^https:\/\/chat\.openai\.com\/(api|backend-api), script-path=https://raw.githubusercontent.com/ourfor/TampermonkeyScript/master/ChatGPTCustom.js, requires-body=true, timeout=10, enable=true
